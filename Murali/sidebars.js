bfdca9619ac8788b00275e3009ed2641d1b9ade1
Murali3007 <murali.kathirmani@gmail.com>
2026-09-06 13:03:35 +0530

    Updated the actual file

--- Murali/sidebars.js
+++ Murali/sidebars.js
@@ -1,10 +1,5 @@
 const sidebars = {
   docsSidebar: [
-    'getting-started',
-    'installation',
-    'user-guide',
-    'administration',
-    'troubleshooting',
     'Tap_to_Pay_User_Guide_2026',
     'WhatsApp_User_Guide_2026',
   ],
